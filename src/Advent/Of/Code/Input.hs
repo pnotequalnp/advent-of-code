3c857b6f191f381f50307b345806dc9f3b5bf042
Kevin Mullins <kevin@pnotequalnp.com>
2021-12-02 07:30:19 -0800

    Add more input processing utilities

--- src/Advent/Of/Code/Input.hs
+++ src/Advent/Of/Code/Input.hs
@@ -1,6 +1,16 @@
 module Advent.Of.Code.Input
-  ( list,
+  ( customList,
+    customList',
+    customNonEmpty,
+    customNonEmpty',
+    list,
     nonEmpty,
+    plainList,
+    plainNonEmpty,
+    wordsList,
+    wordsList',
+    wordsNonEmpty,
+    wordsNonEmpty',
   )
 where
 
@@ -9,8 +19,38 @@ import Data.List.NonEmpty qualified as NE
 import Data.Text (Text)
 import Data.Text qualified as T
 
+plainNonEmpty :: Text -> NonEmpty Text
+plainNonEmpty = NE.fromList . plainList
+
+plainList :: Text -> [Text]
+plainList = customList id
+
 nonEmpty :: Read a => Text -> NonEmpty a
 nonEmpty = NE.fromList . list
 
 list :: Read a => Text -> [a]
 list = fmap (read . T.unpack) . T.lines
+
+customNonEmpty :: (Text -> a) -> Text -> NonEmpty a
+customNonEmpty f = NE.fromList . customList f
+
+customList :: (Text -> a) -> Text -> [a]
+customList f = fmap f . T.lines
+
+customNonEmpty' :: Applicative f => (Text -> f a) -> Text -> f (NonEmpty a)
+customNonEmpty' f = fmap NE.fromList . customList' f
+
+customList' :: Applicative f => (Text -> f a) -> Text -> f [a]
+customList' f = traverse f . T.lines
+
+wordsNonEmpty :: ([Text] -> a) -> Text -> NonEmpty a
+wordsNonEmpty f = NE.fromList . wordsList f
+
+wordsList :: ([Text] -> a) -> Text -> [a]
+wordsList f = fmap (f . T.words) . T.lines
+
+wordsNonEmpty' :: Applicative f => ([Text] -> f a) -> Text -> f (NonEmpty a)
+wordsNonEmpty' f = fmap NE.fromList . wordsList' f
+
+wordsList' :: Applicative f => ([Text] -> f a) -> Text -> f [a]
+wordsList' f = traverse (f . T.words) . T.lines
